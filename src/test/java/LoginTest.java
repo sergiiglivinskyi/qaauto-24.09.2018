712ccb591f31b0a9f776fd05ff85029ed4de5cff
SergiiGlivinskyi <sergii.glivinskyi@gmail.com>
2018-10-12 00:32:46 +0300

    Lesson_4 HomeWork "successfulLoginTest" implementation

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 public class LoginTest {
 
@@ -21,10 +24,25 @@ public class LoginTest {
      * - Close FF browser
      */
     @Test
-    public void successfulLoginTest(){
+    public void successfulLoginTest() {
         WebDriver webDriver = new FirefoxDriver();
         webDriver.navigate().to("https://linkedin.com/");
+
+        WebElement emailField = webDriver.findElement(By.xpath("//input[@class='login-email']"));
+        WebElement passwordField = webDriver.findElement(By.xpath("//input[@class='login-password']"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
+
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/", "Home Page URL is wrong");
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Log In or Sign Up", "Title is wrong");
+        Assert.assertEquals(signInButton.isDisplayed(), true, "Sign In button is absent");
+
+        emailField.sendKeys("sergii.glivinskyi@gmail.com");
+        passwordField.sendKeys("Tszyus1986@");
+        signInButton.click();
+
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home Page URL is wrong");
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn", "Title is wrong");
+
         webDriver.quit();
     }
 }
